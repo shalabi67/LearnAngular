5deeb24050d01a2de54b8c0c11d10d4ced183a9c
mshalabi <mohamad.shalabi@mytoys.de>
2019-06-07 09:11:56 +0200

    refactor components: lessons list refactored.

--- rxjs/learn-angual-reactive/src/app/lessons-list/lessons-list.component.ts
+++ rxjs/learn-angual-reactive/src/app/lessons-list/lessons-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Lesson} from "../models/lesson";
 import {lessonsStore} from "../datalayer/lessons-store";
 import {Observer} from "rxjs";
@@ -8,42 +8,11 @@ import {Observer} from "rxjs";
   templateUrl: './lessons-list.component.html',
   styleUrls: ['./lessons-list.component.css']
 })
-export class LessonsListComponent implements OnInit, Observer<Lesson[]> {
-  private lessons: Lesson[] = [];
+export class LessonsListComponent implements OnInit{
+  @Input()
+  lessons: Lesson[];
 
-  constructor() {
-    this.lessons = [];
+  ngOnInit(): void {
   }
 
-  ngOnInit() {
-    lessonsStore.lessonsList$.subscribe(this);
-  }
-
-  next = (data: Lesson[]) => {
-    console.log('Lessons list component received data ..');
-    this.lessons = data;
-  }
-  /*
-  next(data: Lesson[]) {
-    console.log('Lessons list component received data ..');
-    this.lessons = data;
-  }
-  */
-
-  toggleLessonViewed(lesson:Lesson) {
-    console.log('toggling lesson ...');
-    lessonsStore.toggleLessonViewed(lesson);
-  }
-
-  delete(deleted:Lesson) {
-    lessonsStore.deleteLesson(deleted);
-  }
-
-  closed: boolean;
-  complete = () => {
-    console.log("getting lessons completed.")
-  };
-  error =  (err: any) => {
-    console.log("LessonsListComponent: error while getting lessons " + err);
-  };
 }
